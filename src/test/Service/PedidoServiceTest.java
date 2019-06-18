f473cc08e2b95c996c466a83498eabed4899d6c1
fabriccxio <fquiroga.070689@gmail.com>
2019-06-18 19:57:13 -0300

    Se sacan archivos de prueba.No sirven.

--- src/test/Service/PedidoServiceTest.java
+++
@@ -1,9 +0,0 @@
-package Service;
-
-import org.testng.annotations.Test;
-
-public class PedidoServiceTest {
-  @Test
-  public void f() {
-  }
-}
--- src/test/service/PedidoServiceTest.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import org.testng.annotations.Test;
-
-public class PedidoServiceTest {
-  @Test
-  public void f() {
-  }
-}
